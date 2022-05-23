efa57d3f9d75f96958ac566c17665c6756fbdc91
Muhammad Alif Darmamulia <59762698+alif338@users.noreply.github.com>
2022-05-23 12:42:28 +0000

    Restructured variables and methods of Contract

--- treaceability.sol
+++ treaceability.sol
@@ -3,15 +3,10 @@ pragma solidity ^0.8.2;
 pragma experimental ABIEncoderV2;
 
 contract Treaceability {
-  State public state;
-  mapping (int => Mentah) p_mentah_bank;
-  mapping (int => Jadi) p_jadi_bank;
-  mapping (int => Stup) stup_bank;
-  mapping (int => Peternak) peternak_bank;
   
   struct Stup {
     int id;
-    string[] lokasi;
+    string[] lokasi; // array of float
     string link_foto;
     int terakhir_dipanen;
     string produktivitas; // float
@@ -46,42 +41,66 @@ contract Treaceability {
     Jadi[] products;
   }
 
-  struct State {
-    Stup[] stups;
-    Peternak[] peternaks;
-    RumahProduksi rumah_produksi;
-    Jadi[] konsumen;
-    int[][] graf;
+  mapping (int => Mentah) p_mentah_bank;
+  mapping (int => Jadi) p_jadi_bank;
+  mapping (int => Stup) stup_bank;
+  mapping (int => Peternak) peternak_bank;
+  
+  Stup[] private stup_arr;
+  Peternak[] private peternak_arr;
+  RumahProduksi private rumah_produksi;
+  Jadi[] private konsumen;
+  int[][] private graf;
+
+  // Penambahan data STUP
+  function addStup(string[] memory lokasi, string memory link_foto, int terakhir_dipanen, string memory produktivitas) public {
+      int stup_id = int(stup_arr.length) + 1;
+      Stup memory _stup = Stup(stup_id, lokasi, link_foto, terakhir_dipanen, produktivitas);
+      stup_bank[stup_id] = _stup;
+      stup_arr.push(_stup);
   }
 
-  // Penulisan Data
-  function f1_movedProduct(State memory st, int src, int dest, int id) public {
+  function getStups() public view returns (Stup[] memory) {
+      return stup_arr;
+  }
+
+  function getStupById(int id) public view returns (Stup memory) {
+      return stup_bank[id];
+  }
+
+  // Penambahan data PETERNAK
+  function addPeternak() public {
     
   }
 
-  function f2_addStupStatus(State memory st, int id_stup, Stup memory new_data) public {
+  // Perubahan Data ==========================================================
+  function f1_movedProduct(int src, int dest, int id) public {
     
   }
 
-  function f3_changeRumahProduksiStatus(State memory st, RumahProduksi memory new_data) public {
+  function f2_addStupStatus(int id_stup, Stup memory new_data) public {
     
   }
 
-  function f4_farming(State memory st, int[] memory stup_sumber, int id_peternak, string[] memory komposisi_volume) public {
+  function f3_changeRumahProduksiStatus(RumahProduksi memory new_data) public {
     
   }
 
-  // Pembacaan data
-  function g1_getStupInfo(State memory st, int id_stup) public view returns (Stup memory) {
-    return stup_bank[id_stup];
-  }
-
-  function g2_getPeternakInfo(State memory st, int id_peternak) public view returns (Peternak memory) {
+  function f4_farming(int[] memory stup_sumber, int id_peternak, string[] memory komposisi_volume) public {
     
   }
 
-  function g3_getRumahProduksiInfo() {
-    
+  // Pembacaan data ========================================================
+  function g1_getStupInfo(int id_stup) public view returns (Stup memory) {
+   return stup_bank[id_stup];
+  }
+
+  function g2_getPeternakInfo(int id_peternak) public view returns (Peternak memory) {
+    return peternak_bank[id_peternak];
+  }
+
+  function g3_getRumahProduksiInfo() public view returns (RumahProduksi memory) {
+    return rumah_produksi;
   }
   
 }
